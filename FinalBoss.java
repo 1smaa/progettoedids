8fd8904c0bb29f7c08c80c574536521b7c71ee92
1smaa <77068062+1smaa@users.noreply.github.com>
2024-06-27 14:40:52 +0200

    Delete FinalBoss.java

--- FinalBoss.java
+++
@@ -1,7 +0,0 @@
-public class FinalBoss implements CallBack{
-    private final Entity boss=new Entity("Pier",20,4,0);
-    public boolean onCallback(Entity player,RoomMap map,Labirinth labirinth){
-        Combat c=new Combat(this.boss,player);
-        return c.start()==1;
-    }
-}
